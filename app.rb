af6e9b67bc0d57bac23c59d56c8cd7ee8d4346a4
Russell <russellmorbey@hotmail.co.uk>
2022-05-04 10:33:37 +0100

    Steps #1 to #9 Complete.

---
+++ app.rb
@@ -0,0 +1,18 @@
+require 'sinatra'
+require "sinatra/reloader" if development?
+
+get '/' do
+  "hello!"
+end
+
+get '/secret' do
+  "This is a secret."
+end
+
+get '/response' do
+  "Haha, this is jokes."
+end
+
+get '/cat' do
+  erb(:index)
+end
